bfdb458738861cc3d221e9a70fca14c88ec6c9ef
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-14 09:05:39 -0600

    merging the files together with the master

--- FinalProject/AppNavigator.js
+++
@@ -1,9 +0,0 @@
-import { createStackNavigator } from 'react-navigation';
-// import productsDetails from './components/products-details';
-import App from './main';
-
-const AppNavigator = createStackNavigator({
-  App: { screen: App },
-});
-
-export default AppNavigator;
