162f085b15064f1c2eae31f550772422b5aa2ef1
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-16 20:22:57 -0600

    used axios to get data from api endpoint, not successful yet

--- bugi/components/products-details.js
+++ bugi/components/products-details.js
@@ -2,7 +2,17 @@ import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { Actions } from "react-native-router-flux";
 import { navigation } from "@react-navigation/stack";
+import axios from "axios";
+
 const ProductsDetails = ({ navigation: { navigate } }) => {
+  axios.get(`http://localhost:19003/api/product-details`)
+  .then((response) => {
+    console.log("here")
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
   return (
     <View style={styles.container}>
       <Button title="Continue shopping" onPress={() => navigate("QRScanner")} />
